d1372d102c60bbd336d84c63e247dffe092a7369
Matthias Jansen <matthias.jansen@outlook.de>
2017-04-23 13:31:05 +0200

    Refactored the namespaces/nesting;

--- readme.md
+++ readme.md
@@ -1,6 +1,30 @@
-# ExceptionManager
+# Exceptional
+
+Exceptional is a exception manager, with the simple goal of making exception management exceptionally simple.
+
+
+
 ## Responsibilities
 ## Particularities
+
+### About
+
+#### Policy Groups
+
+#### Policies
+
+#### Handlers
+
+A handler is pretty much a conversion step in the pipeline.
+It takes an exception and returns a different one.
+This can be an exception of the same type.
+
+Handlers are meant to be stateless, however nothing prevents you from creating a handler that has state.
+Keep in mind however that such a handler needs to be thread safe.
+
+#### Terminators
+
+Whenever the exception resulting from a policy is not supposed to be thrown in the end, a terminator is required.
 
 ### Examples
 
---
+++ src/Exceptional/Installer/PolicyGroupInstaller.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exceptional.Builder;
+using Exceptional.Policies;
+using JetBrains.Annotations;
+
+namespace Exceptional.Installer
+{
+    public abstract class PolicyGroupInstaller<TSrc, TDst>
+        where TSrc : Exception
+        where TDst : Exception
+    {
+        private readonly List<Func<PolicyDefinitionBuilder<TSrc, TDst>, CompletePolicyDefinition<TSrc, TDst>>> ctxCreators = 
+            new List<Func<PolicyDefinitionBuilder<TSrc, TDst>, CompletePolicyDefinition<TSrc, TDst>>>();
+
+        private Func<DefaultPolicyDefinitionBuilderHead<TSrc, TDst>, CompletePolicyDefinition<TSrc, TDst>> defCreator;
+
+        public ExceptionPolicyGroup<TSrc, TDst> Provide()
+        {
+            Define();
+            return PolicyGroupBuilder.Create(defCreator, ctxCreators.ToArray());
+        }
+
+        public abstract void Define();
+
+        protected void DefineDefaultPolicy([NotNull] Func<DefaultPolicyDefinitionBuilderHead<TSrc, TDst>, CompletePolicyDefinition<TSrc, TDst>> defPolicy)
+        {
+            if (defPolicy == null)
+            {
+                throw new ArgumentNullException(nameof(defPolicy));
+            }
+            if (defCreator != null)
+            {
+                throw new InvalidOperationException("The default policy has already been defined.");
+            }
+
+            defCreator = defPolicy;
+        }
+
+        protected void DefinePolicy(string context, [NotNull]
+            Func<PolicyDefinitionBuilder<TSrc, TDst>, CompletePolicyDefinition<TSrc, TDst>> ctxPolicy)
+        {
+            if (ctxPolicy == null)
+            {
+                throw new ArgumentNullException(nameof(ctxPolicy));
+            }
+            ctxCreators.Add(ctxPolicy);   
+        }
+    }
+}
