1f9afa6c4c918366a736a093b6866e0a9d0e4c7a
Matthias Jansen <matthias.jansen@outlook.de>
2017-06-11 12:53:07 +0200

    Started an explorational project, for demonstrations;

---
+++ Exceptional.Exploration/Entry.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using Exceptional.Handlers;
+using Exceptional.Installer;
+using Exceptional.Installer.Builder;
+using Exceptional.Policies;
+
+namespace Exceptional.Exploration
+{
+    public class AccessViolationPolicyInstaller : PolicyGroupInstaller<AccessViolationException, Exception>
+    {
+        protected override CompletePolicyDefinition<AccessViolationException, Exception> Provide(
+            DefaultPolicyDefinitionBuilderHead<AccessViolationException, Exception> builder)
+        {
+            return builder.StartAndComplete(new ExceptionHandler<AccessViolationException, Exception>());
+        }
+
+        protected override IEnumerable<CompletePolicyDefinition<AccessViolationException, Exception>> Provide(
+            PolicyDefinitionBuilder<AccessViolationException, Exception> builder)
+        {
+            yield return builder
+                .SetContext("sql")
+                .StartAndComplete(new ExceptionHandler<AccessViolationException, Exception>());
+
+            yield return builder
+                .SetContext("wmi")
+                .StartAndComplete(new ExceptionHandler<AccessViolationException, Exception>());
+        }
+    }
+
+    public class Entry
+    {
+        public static void Main(string[] args)
+        {
+            ExceptionManagerConfiguration.AddPolicyGroupFrom<AccessViolationPolicyInstaller>();
+
+            var manager = ExceptionManagerConfiguration.LockAndCreateManager();
+
+            try
+            {
+                File.Open("d05c746801b147e281ef52e6dfbfc781", FileMode.Open);
+            }
+            catch (Exception e)
+            {
+                manager.Handle(e);
+            }
+
+        }
+    }
+}
