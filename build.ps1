593e76fe4872d462bad584196d4f7b1e76148aad
Matthias Jansen <matthias.jansen@outlook.de>
2017-12-10 13:53:24 +0100

    Moving on to appveyor;

---
+++ build.ps1
@@ -0,0 +1,1 @@
+iex ((new-object net.webclient).DownloadString('https://gist.githubusercontent.com/MatthiasJansen/d7d67046bf5fa10cd4d2f4f386b99b53/raw/43c99e7c1e1c73c1dfb19725075e7ade4d6ac7a7/build.ps1'))
